0250a49898999a2d9407b07db554c846b4f3811b
rixshavv <23BCS12941@cuchd.in>
2025-07-04 19:27:38 +0530

    Added optimization case using two variables and eliminating using stack

--- Stacks/minAddtoMakeValidBracketPair.java
+++ Stacks/minAddtoMakeValidBracketPair.java
@@ -22,5 +22,28 @@ public class minAddtoMakeValidBracketPair {
         }
         return stack.size(); //the remaining invalid pairs must be again added with opening to make it valid pair 
     }
+
     
 }
+
+//optimization
+
+
+    //      int open=0,close=0;
+    
+    //     for(int i=0;i<s.length();i++){
+    //         char ch=s.charAt(i);
+    //         if(ch=='{'){
+    //             open++;
+    //         }else{
+    //             if(open<=0){
+    //                 close++;
+    //             }else{
+    //                open--;
+    //             }
+
+    //         }
+    //     }
+    //     return open+close; //the remaining invalid pairs must be again added with opening to make it valid pair 
+    // }
+
